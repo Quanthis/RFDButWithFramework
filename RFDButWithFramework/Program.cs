70f31222cf2ea1bb885f5e5356daea4a4a834c20
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2019-11-14 13:57:51 +0100

    Zmiany

--- RFDButWithFramework/Program.cs
+++ RFDButWithFramework/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+//using static System.Data.Common.DbConnection;
+using static System.Console;
+//using System.Data;
 
-namespace RFDButWithFramework
+namespace ReadingFromDatabase
 {
     class Program
     {
+        static private string GetConnectionString()
+        {
+            return "Data Source=DESKTOP-EPR1T48;Initial Catalog=TSQLV4;" + "Integrated Security=true;"; 
+        }
+
+        private static void OpenSQLConnection()
+        {
+            string connectionString = GetConnectionString();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection())
+                {
+                    con.ConnectionString = connectionString;
+                    con.Open();
+                    WriteLine($"State: {con.State}");
+                    WriteLine($"ConnectionString: {con.ConnectionString}");
+
+                }
+            }
+            catch (Exception sex)
+            {
+                WriteLine("Exception code:" + sex);
+            }
+        }
+
+        /*private static void CreateCommand(string querryString, string connectionString)
+        {
+            using (SqlConnection conneciton = new SqlConnection(connectionString))
+            {
+
+            }
+        }*/
+
+
+
+
+
         static void Main(string[] args)
         {
+            OpenSQLConnection();
+            ReadKey();
         }
     }
 }
